ee6801197ea234371cf4d235c9fb33fef0320f94
Hector Atilano <159952379+daniel-Hector@users.noreply.github.com>
2024-04-27 15:44:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#To do App
+...
+## Technologies ##
+* __HTML__
+* __CSS__
+* __Javascript__
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: development,
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        watchFiles: ['./src/index.html'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    entry: './src/index.js',
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        },``
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
+      },
+};
