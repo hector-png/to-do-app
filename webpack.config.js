765f58b2c3b75c03f2f065386c4be910f3f866bc
Hector Atilano <159952379+daniel-Hector@users.noreply.github.com>
2024-04-28 08:38:23 -0600

    Fix typos

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: development,
+    mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
@@ -30,7 +30,4 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    optimization: {
-        runtimeChunk: 'single',
-      },
 };
