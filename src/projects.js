cb6824a02b06933f27c37f005fbf43f54b92201d
Hector Atilano <159952379+daniel-Hector@users.noreply.github.com>
2024-05-01 15:20:01 -0600

    Seperate different logic for maintainability

---
+++ src/projects.js
@@ -0,0 +1,28 @@
+class Project {
+    projectTaskList = [];
+
+    constructor(name, label = 'default') {
+        this.name = name;
+        this.label = label;
+    }
+
+    get projectName() {
+        return this.name;
+    }
+
+    set projectName(value) {
+        this.projectName = value;
+    }
+
+    get taskList() {
+        return this.projectTaskList;
+    }
+
+    addTask(task) {
+        this.projectTaskList.push(task);
+    }
+
+    removeTask(targetTask) {
+        this.projectTaskList = this.projectTaskList.filter((task) => task != targetTask);
+    }
+};
