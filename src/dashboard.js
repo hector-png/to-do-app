bc11c7b792d5da18eac9e39c85e6079f124e5a02
Hector Atilano <159952379+daniel-Hector@users.noreply.github.com>
2024-05-20 21:09:31 -0600

    Add dashboard DOM js

---
+++ src/dashboard.js
@@ -0,0 +1,17 @@
+import { Manager } from "./projectManager.js";
+import Project from "./projects.js";
+import Task from "./tasks.js";
+
+function defaultTodoList() {
+    const defaultTodoList = new Project('Todo List');
+    
+    
+}
+
+function createDashboard() {
+    const projectManager = Manager;
+
+    const render = () => {
+        
+    };
+}; 
